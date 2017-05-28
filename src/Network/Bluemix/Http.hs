4c53928b5207caddcd47abfd6683fa855c4a8b2c
Alexander Thiemann <mail@athiemann.net>
2017-05-28 02:29:40 +0200

    improve error reporting

--- src/Network/Bluemix/Http.hs
+++ src/Network/Bluemix/Http.hs
@@ -11,13 +11,14 @@ import Network.Bluemix.Auth
 import Data.Aeson (eitherDecode, encode, ToJSON, FromJSON)
 import Network.HTTP.Client
 import Network.HTTP.Types
+import qualified Data.ByteString.Lazy as BSL
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
 
 data Result r
     = ROkay r
     | RBadResponse !T.Text
-    | RBadStatus !Status
+    | RBadStatus !Status !BSL.ByteString
     deriving (Show, Eq)
 
 runReq :: (ToJSON req, FromJSON r) => Method -> Auth a -> T.Text -> req -> IO (Result r)
@@ -41,4 +42,4 @@ runReq meth auth url requestObject =
              case eitherDecode (responseBody response) of
                Left errMsg -> pure (RBadResponse $ T.pack errMsg)
                Right ok -> pure (ROkay ok)
-         _ -> pure $ RBadStatus rs
+         _ -> pure $ RBadStatus rs (responseBody response)
